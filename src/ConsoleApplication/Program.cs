85c7b5fb645a38def85e18c2a4f188dd30509389
Terry Hughes <terry@tnthughes.net>
2016-03-20 20:17:11 -0700

    Simple console app

---
+++ src/ConsoleApplication/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, you passed in the following arguments:");
+
+            foreach (var arg in args)
+                Console.WriteLine("\t{0}", arg);
+
+            Console.WriteLine("Goodbye");
+        }
+    }
+}
